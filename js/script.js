9287dbfc3857b22bd288ff8f5c304e2116332592
Joshua ifede <Joshuaifede@gmail.com>
2025-05-10 03:49:35 -0700

    updated the style of the website

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({
-      duration: 1000, // Animation duration in ms
-      easing: 'ease-in-out',
-      once: true // Animation happens only once
-    });
+  AOS.init({
+    duration: 1000, // Animation duration in ms
+    easing: 'ease-in-out',
+    once: true // Animation happens only once
   });
+});
 
-  // Counter Animation Script
-  document.addEventListener("DOMContentLoaded", () => {
-    const counters = document.querySelectorAll(".stat-number");
-    
-    counters.forEach(counter => {
-      const updateCount = () => {
-        const target = +counter.getAttribute("data-target");
-        const count = +counter.innerText;
-        const increment = target / 100; // Increased the divisor to slow down the animation
+// Counter Animation Script
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".stat-number");
 
-        if (count < target) {
-          counter.innerText = Math.ceil(count + increment);
-          setTimeout(updateCount, 70); // Increased the timeout to slow down the animation
-        } else {
-          counter.innerText = target;
-        }
-      };
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+      const increment = target / 100; // Adjust the divisor to control speed
 
-      updateCount();
-    });
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+      } else {
+        counter.innerText = 0; // Reset to 0 when the target is reached
+      }
+      setTimeout(updateCount, 70); // Adjust the timeout to control speed
+    };
+
+    updateCount();
   });
+});
